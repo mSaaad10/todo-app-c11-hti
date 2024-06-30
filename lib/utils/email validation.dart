26959ab33b7d0ce9a89a7be1b8e2f86c9beeafd8
MuhammedSaad <muhammedsaad.dev@gmail.com>
2024-06-30 19:51:39 +0300

    Revert "+add task to fire store"
    This reverts commit 6e2c4bde78ab043529512f7ba63d6067f491bb63.

--- lib/utils/email validation.dart
+++
@@ -1,7 +0,0 @@
-bool isValidEmail(String email){
-
- return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-      .hasMatch(email);
-
-  //muhammedSaad11.trainning1@route10.com
-}
